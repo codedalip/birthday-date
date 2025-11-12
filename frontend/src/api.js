b5f3bbdc106945c843cece647279374a232b10c6
Dalip <drdzcreations@gmail.com>
2025-11-12 23:26:59 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 
-const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:5000';
+const API_BASE = import.meta.env.VITE_API_BASE
+const response = await fetch(`${API_BASE}/api/birthdays`);
 const client = axios.create({ baseURL: API_BASE + '/api' });
 
 
